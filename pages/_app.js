5cee3e655c65f08f0563d76aece650fff32d85fd
Taras Bunyk <tbunyk@gmail.com>
2022-10-10 11:43:23 +0200

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,12 @@
 import 'highlight.js/styles/github.css'
 import '../styles.css'
+import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <>
+    <GoogleAnalytics trackPageViews gaMeasurementId="UA-42001280-1"/>
+	<Component {...pageProps} />
+  </>
 }
 
 export default MyApp
