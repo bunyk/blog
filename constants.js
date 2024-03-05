c94acf8b833f2717600ccfaa34bac8bcbbf89743
Taras Bunyk <tbunyk@gmail.com>
2024-03-05 19:25:16 +0100

    Add sudoku game & do some updates

--- constants.js
+++ constants.js
@@ -4,6 +4,7 @@ export const SECTIONS = [
     {url: '/', title: 'Home'},
     {url: 'http://bunyk.github.io/mandala/', title: 'Snowflake drawing'},
     {url: '/resume', title: 'Resume'},
+    {url: '/pages/sudoku.html', title: 'Sudoku fall'},
     // {url: '/about', title: 'About'}, // TODO
     {url: 'https://bunyk.wordpress.com/', title: 'Old blog (in Ukrainian)'},
 ];
--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import 'highlight.js/styles/github.css'
 import '../styles.css'
-import { GoogleAnalytics } from "nextjs-google-analytics";
 
 function MyApp({ Component, pageProps }) {
   return <>
-    <GoogleAnalytics trackPageViews gaMeasurementId="UA-42001280-1"/>
 	<Component {...pageProps} />
   </>
 }
